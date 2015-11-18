586f09547eeca68f3f64b38315d58fe59a8ec1b7
Kiyichii <Kinorose@my.ccsu.edu>
2015-11-18 16:11:55 -0500

    Add phaser canvas

---
+++ client/gamepage/gamepage.js
@@ -0,0 +1,18 @@
+/*Run this code when the template is rendered */
+Template.gamepage.onRendered(function(){
+var game = new Phaser.Game(800, 500, Phaser.AUTO, 'game-area');
+        
+var GameState = {
+  	preload: function() {
+
+  	},
+
+  	create: function() {
+
+  	},
+
+  	update: function() {
+
+  	}
+ };
+});
