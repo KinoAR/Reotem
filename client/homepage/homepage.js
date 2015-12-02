aee952de9018c4003b614e9b0ac40f0304b7d6ed
Kiyichii <Kinorose@my.ccsu.edu>
2015-12-02 02:02:11 -0500

    Fix navigation between multiple pages

--- client/homepage/homepage.js
+++ client/homepage/homepage.js
@@ -1,5 +1,9 @@
-//Load template when link is clicked & specify client / server side -- curly braces represent js object
-  var counter = 0;
+//Load content information when rendered
+  Template.homepage.onRendered(function(template) {
+    template = Template.homepagecontent;
+    Blaze.render(template,Template.instance().find('#content'));
+  });
+
   Template.homepage.events({  	
   	//Check if clicked any element in the homepage navigation
   	'click .homepage-nav' : function(event, template){
@@ -7,44 +11,45 @@
   		event.preventDefault();
   		event.stopPropagation();
 
-  		console.log(clickedElement);
   	//Checked if clicked gamepage link
-  	if(clickedElement.matches('#gamepage-link') && counter < 1){
-  		if(!Template.instance().find('#game-area')){
+  	if(clickedElement.matches('#gamepage-link')) {
+  		if(!Template.instance().find('#game-area')) {
 	  	 //Renders a template on the page & query for the specific dom element
-		    template = Template.gamepage;
-		    Blaze.render(template,Template.instance().find('#content'));
-        counter++;
-		    console.log("Loaded gamepage template");	
+      let view = Blaze.getView(template.find('.content-information'));
+      if(view.name != "Template.homepage") {
+        Blaze.remove(view);
+      }
+		  template = Template.gamepage;
+		  Blaze.render(template,Template.instance().find('#content'));
   		}
   	}
     //Check if clicked author link
-    else if(clickedElement.matches('#authorpage-link') && counter < 1)
-    {
+    else if(clickedElement.matches('#authorpage-link')) {
+      let view = Blaze.getView(template.find('.content-information'));
+      if(view.name != "Template.homepage"){
+        Blaze.remove(view);
+      }
       template = Template.authorpage;
-      Blaze.render(template,Template.instance().find('#content'));
-      counter++;
+      Blaze.render(template,Template.instance().find('#content'))
       console.log("Loaded author template");
     }
-    else if(clickedElement.matches('#signup-link') && counter < 1)
-    {
+    else if(clickedElement.matches('#signup-link')) {
+      let view = Blaze.getView(template.find('.content-information'));
+      if(view.name != "Template.homepage") {
+        Blaze.remove(view); 
+      }
       template = Template.signuppage;
       Blaze.render(template,Template.instance().find('#content'));
-      counter++;
       console.log("Loaded sign up template");
     }
     //Currently breaks if there is no content-information class selectors on the page.
-    else if(clickedElement.matches('#homepage-link'))
-    {
+    else if(clickedElement.matches('#homepage-link')) {
       let view = Blaze.getView(template.find('.content-information'));
-      let viewData = Blaze.getView(template.find('.content-information'));
-      console.log(viewData);
       if(view.name != "Template.homepage"){
         Blaze.remove(view);
-        counter--;
-        console.log('Removed View: ' + view);
-      }   
-    }
-  	
+      }
+      template = Template.homepagecontent;
+      Blaze.render(template,Template.instance().find('#content'));   
+    } 	
   }
 });
